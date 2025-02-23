c777f5832aa00b12c769a91737527860b1cc5bfd
Hafidmualimin <hafidmualimin@gmail.com>
2025-02-23 14:35:06 +0700

    Praktikum 01

--- README.md
+++
@@ -1,6 +0,0 @@
-# pemograman Web 2
-
-Repositori Praktikum Mata Kuliah Pemograman Web 2
-Nama : Abdul Hafid Mualimin
-Nim : 0110124024
-Rombel : Si12
---
+++ variabel_user.php
@@ -0,0 +1,13 @@
+<?php
+// mendifinisikan variabel
+$nama = 'Hafid';
+$umur = 20 ;
+$berat = 55;
+$tinggi_badan = 165;
+
+echo 'nama saya '. $nama;
+echo '<br/>umur:' . $umur . 'tahun';
+echo '<br/>berat:' . $berat . 'kg';
+echo '<br/>tinggi badan:' . $tinggi_badan . 'cm';
+
+?>
